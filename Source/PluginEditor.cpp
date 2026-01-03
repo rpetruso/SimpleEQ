ff68c6650378fab4146aea95bd6851b19a8debe9
Rob Petruso <robpetruso@robs-mbp.mynetworksettings.com>
2026-01-03 15:50:48 -0500

    added frequency labels, thinned out number of frequencie

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -322,9 +322,9 @@ void ResponseCurveComponent::resized()
     
     Array<float> freqs
     {
-        20, 30, 40, 50, 100,
-        200, 300, 400, 500, 1000,
-        2000, 3000, 4000, 5000, 10000,
+        20, /*30, 40,*/ 50, 100,
+        200, /*300, 400,*/ 500, 1000,
+        2000, /*3000, 4000,*/ 5000, 10000,
         20000
     };
     
@@ -364,7 +364,39 @@ void ResponseCurveComponent::resized()
         g.drawHorizontalLine(y, left, right);
     }
     
-    g.drawRect(getAnalysisArea());
+//    g.drawRect(getAnalysisArea());
+    
+    g.setColour(Colours::lightgrey);
+    const int fontheight = 10;
+    g.setFont(fontheight);
+    
+    for( int i =0; i < freqs.size(); ++i)
+    {
+        auto f = freqs[i];
+        auto x = xs[i];
+        
+        bool addK = false;
+        String str;
+        if( f > 999.f)
+        {
+            addK=true;
+            f/=1000.f;
+        }
+        
+        str << f;
+        if ( addK )
+            str << "k";
+        str << "Hz";
+        auto textWidth = g.getCurrentFont().getStringWidth(str); // build rect around string width
+        
+        Rectangle<int> r;
+        r.setSize(textWidth, fontheight);
+        r.setCentre(x, 0);
+        r.setY(1);
+        
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+        
+    }
 }
 
 juce::Rectangle<int> ResponseCurveComponent::getRenderArea()
@@ -375,7 +407,7 @@ juce::Rectangle<int> ResponseCurveComponent::getRenderArea()
 //                  8 //JUCE_LIVE_CONSTANT(5)
 //                  );
     
-    bounds.removeFromTop(12);
+    bounds.removeFromTop(12); // draw text within 12 size gap
     bounds.removeFromBottom(2);
     bounds.removeFromLeft(20);
     bounds.removeFromRight(20);
