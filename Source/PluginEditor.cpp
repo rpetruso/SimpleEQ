a3c580493fd7c936f013b340bfecfae9cd0086dd
Rob Petruso <robpetruso@robs-mbp.mynetworksettings.com>
2026-01-06 17:35:03 -0500

    tweaked button text when enabled oor disabled

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -62,10 +62,10 @@ void LookAndFeel::drawRotarySlider(juce::Graphics & g,
         r.setSize(strWidth + 4, rswl->getTextHeight() + 2); // rect a bit bigger than bounds for text
         r.setCentre(bounds.getCentre()); // center of rect center of bounds
         
-        g.setColour(Colours::black);
+        g.setColour(enabled ? Colours::black : Colours::darkgrey);
         g.fillRect(r);
         
-        g.setColour(Colours::white);
+        g.setColour(enabled ? Colours::white : Colours::lightgrey);
         g.drawFittedText(text, r.toNearestInt(), juce::Justification::centred, 1);
     }
     
