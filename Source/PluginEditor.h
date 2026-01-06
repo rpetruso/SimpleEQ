188d57bf8042d335ca2bb2f5a244add4f47394f3
Rob Petruso <robpetruso@robs-mbp.mynetworksettings.com>
2026-01-06 16:46:25 -0500

    Analyzere button is visible, path is recreated every time repaints

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -270,6 +270,9 @@ private:
     PathProducer leftPathProducer, rightPathProducer;
 };
 //==============================================================================
+struct PowerButton : juce::ToggleButton { };
+struct AnalyzerButton : juce::ToggleButton { };
+
 /**
 */
 class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor
@@ -310,7 +313,8 @@ private:
                 lowCutSlopeSliderAttachment,
                 highCutSlopeSliderAttachment;
     
-    juce::ToggleButton lowcutBypassButton, peakBypassButton, highcutBypassButton, analyzerEnabledButton;
+    PowerButton lowcutBypassButton, peakBypassButton, highcutBypassButton;
+    AnalyzerButton analyzerEnabledButton;
     
     using ButtonAttachment = APVTS::ButtonAttachment;
     ButtonAttachment lowcutBypassButtonAttachment,
